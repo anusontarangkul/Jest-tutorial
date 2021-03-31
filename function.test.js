fb6e2a8e9cbf567e082e411eb6dd0e69151192f1
David Anusontarangkul <anusontarangkul.d@gmail.com>
2021-03-30 23:13:55 -0700

    test matchers

---
+++ function.test.js
@@ -0,0 +1,37 @@
+const functionsT = require("./function")
+
+test("Adds 2 + 2 to equal 4", () => {
+    expect(functionsT.add(2, 2)).toBe(4)
+})
+
+test("Should be null", () => {
+    expect(functionsT.isNull()).toBeNull()
+})
+
+test("Should be falsy", () => {
+    expect(functionsT.checkValue(undefined)).toBeFalsy()
+})
+
+test("User should be Brad Traversy object", () => {
+    expect(functionsT.createUser()).toEqual({
+        firstName: "Brad",
+        lastName: "Traversy"
+    })
+})
+
+test("Should be under 1600", () => {
+    const load1 = 800;
+    const load2 = 700;
+    expect(load1 + load2).toBeLessThanOrEqual(1600);
+})
+
+// Regex
+test("There is no I in team", () => {
+    expect("team").not.toMatch(/I/);
+});
+
+// Arrays
+test("Admin should be in usernames", () => {
+    usernames = ["john", "karen", "admin"];
+    expect(usernames).toContain("admin")
+})
